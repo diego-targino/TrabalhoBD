e7a6b7efe07e8e04cbf687ee3321e10471cc1f7c
luisgbr1el <harrycorpmail@gmail.com>
2023-06-24 18:46:10 -0300

    do not allow blank inputs to save

--- src/main/java/br/ufc/coop/trabalhobd/Repositories/BaseRepository.java
+++ src/main/java/br/ufc/coop/trabalhobd/Repositories/BaseRepository.java
@@ -6,7 +6,7 @@ public abstract class BaseRepository<T> {
 
     protected static String DB_URL = "jdbc:mysql://localhost/school";
     protected static String DB_USER = "root";
-    protected static String DB_PASSWORD = "";
+    protected static String DB_PASSWORD = "root";
 
     public abstract void Insert(T entity);
 
